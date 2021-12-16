f935a5d63d6274488e552891ad4621b02755b773
Artur Sarakhman <74RrBzH2YP>
2021-12-16 13:18:18 +0200

    Initial commit

---
+++ src/main/java/com/brawlstars/asylum/AsylumApplication.java
@@ -0,0 +1,13 @@
+package com.brawlstars.asylum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsylumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsylumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brawlstars/asylum/AsylumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brawlstars.asylum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsylumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
