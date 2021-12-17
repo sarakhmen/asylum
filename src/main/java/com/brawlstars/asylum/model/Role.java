31b215984142471856a1b24f21fdec60c7355018
chbog <83748023+chbog@users.noreply.github.com>
2021-12-17 15:59:34 +0200

    role and user models

---
+++ src/main/java/com/brawlstars/asylum/model/Role.java
@@ -0,0 +1,19 @@
+package com.brawlstars.asylum.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role")
+    private String role;
+}
+
