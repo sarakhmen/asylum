544da1af87b042a6fe1d1aec0b0792237ca2186c
Kirill Sokirka <sokirka2003@gmail.com>
2021-12-19 02:16:29 +0200

    Add simple repo and service

---
+++ src/main/java/com/brawlstars/asylum/controller/AppointmentController.java
@@ -0,0 +1,20 @@
+package com.brawlstars.asylum.controller;
+
+import com.brawlstars.asylum.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AppointmentController {
+
+    @Autowired
+    AppointmentService;
+    @GetMapping("??")
+    public String appointmentsView(HttpServletRequest httpServletRequest) {
+        //??
+        return "appointment";
+    }
+}
